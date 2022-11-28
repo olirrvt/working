9c43c3d1a533a3ebb43dbb5d395957de9010c34a
Taylor Oliveira <taylorso2004@hotmail.com>
2022-11-28 07:00:48 -0300

    fetch de login

--- public/js/fetch/login.js
+++ public/js/fetch/login.js
@@ -1,18 +1,30 @@
-// const input = document.getElementsByTagName("input");
+const input = document.getElementsByTagName("input");
+const url = "http://localhost:3015/login";
 
-// function logar() {
-//      const emailUser = input[2].value.trim();
-//      const senhaUser = input[3].value.trim();
+const logar = async () => {
 
-//      const url = "http://localhost:3015/login";
-//      await fetch(url, {
-//           method: "POST",
-//           headers: {
-//                "Content-Type": "application/json"
-//           },
-//           body: JSON.stringify({
-//                email: emailUser,
-//                senha: senhaUser
-//           })
-//      })
-// };
+    const emailUser = input[2].value.trim();
+    const senhaUser = input[3].value.trim();
+
+    await fetch(url, {
+         method: "POST",
+         headers: {
+              "Content-Type": "application/json"
+         },
+         body: JSON.stringify({
+              email: emailUser,
+              senha: senhaUser
+         })
+    })
+    .then(res => res.json())
+    .then(res => {
+        const usuario = res.usuario;
+        // Constante com o usuário
+        if (usuario) {
+            window.location.href = "http://localhost:3040/"
+        } else {
+            console.log("Erro!")
+            // Passar mensagem de erro ao usuário!
+        }
+    })
+};
